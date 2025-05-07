cb09b41954ca204796ea88ebceb95959373b4832
sseriw <165750913+sseriw@users.noreply.github.com>
2025-05-07 14:30:57 +0600

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-todo-example'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -63,6 +63,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
